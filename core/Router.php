6702024438b7ac628159a83edcbb1f0c0c10d4d5
Paweł Stempak <pawelstempak@gmail.com>
2022-04-13 00:07:16 +0200

    msp0.0.2

---
+++ core/Router.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace app\core;
+
+class Router
+{
+    public Request $request;
+    protected array $routes = [];
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function get($path, $callback)
+    {
+        $this->routes['get'][$path] = $callback;
+    }
+
+    public function resolve()
+    {
+        $path = $this->request->getPath();
+        $method = $this->request->getMethod();             
+    }
+}
