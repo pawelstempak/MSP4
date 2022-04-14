0efa6231a34c125789eef79d21c046ae2db670b7
Paweł Stempak <pawelstempak@gmail.com>
2022-04-14 23:10:54 +0200

    msp0.0.5

--- core/Router.php
+++ core/Router.php
@@ -5,11 +5,13 @@ namespace app\core;
 class Router
 {
     public Request $request;
+    public Response $response;
     protected array $routes = [];
 
-    public function __construct(Request $request)
+    public function __construct(Request $request, Response $response)
     {
         $this->request = $request;
+        $this->response = $response;
     }
 
     public function get($path, $callback)
@@ -22,9 +24,10 @@ class Router
         $path = $this->request->getPath();
         $method = $this->request->getMethod();  
         $callback = $this->routes[$method][$path] ?? false;
-        if($callback === false) {
+        if($callback === false) 
+        {
+            $this->response->setStatusCode(404);
             return "Not found";
-            exit;
         }       
 
         if(is_string($callback))
@@ -36,14 +39,22 @@ class Router
 
     protected function layoutContent()
     {
+        ob_start();
         include_once Application::$ROOT_DIR . "/views/layouts/main.php";
+        return ob_get_clean();
     }
     
     public function renderView($view)
     {
         $layoutContent = $this->layoutContent();
-
-        include_once Application::$ROOT_DIR . "/views/$view.php";
+        $viewContent = $this->renderOnlyView($view);
+        return str_replace('{{content}}', $viewContent, $layoutContent);
     }
-
+    
+    protected function renderOnlyView($view)
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR . "/views/$view.php";
+        return ob_get_clean();
+    }
 }
--- public/index.php
+++ public/index.php
@@ -3,10 +3,8 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use app\core\Application;
-use app\core\Router;
-use app\core\Request;
 
-$app = new Application(dirname(__DIR__));
+$app = new Application(dirname(__DIR__)); 
 
 $app->router->get('/', 'home');
 
