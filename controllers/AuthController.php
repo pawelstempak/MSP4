0fd772639e267daa31bbcd51c6d8ebb24a94a027
Paweł Stempak <pawelstempak@gmail.com>
2022-04-18 22:57:43 +0200

    msp0.0.10

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -2,6 +2,7 @@
 namespace app\controllers;
 use app\core\Controller;
 use app\core\Request;
+use app\models\RegisterModel;
 
 class AuthController extends Controller
 {
@@ -13,11 +14,27 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
+        $registerModel = new RegisterModel();
         if($request->isPost())
         {
-            return 'Handle submited data';
+            $registerModel->loadData($request->getBody());
+
+            var_dump($registerModel);
+
+            if($registerModel->validate() && $regiterModel->register())
+            {
+                return 'Success';
+            }
+            
+            // $firstname = $request->getBody()['firstname'];
+            // if(!$firstname)
+            // {
+            //     $errors['firstname'] = 'This field is required.';
+            // }
         }
         $this->setLayout('auth');
-        return $this->render('register');
+        return $this->render('register', [
+            'model' => $registerModel
+        ]);
     }    
 }
---
+++ models/RegisterModel.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\models;
+use app\core\Model;
+
+class RegisterModel extends Model
+{
+    public string $firstname;
+    public string $lastname;
+    public string $email;
+    public string $password;
+    public string $confirmPassword;
+
+    public function register()
+    {
+        echo "Crating new user";
+    }
+}
