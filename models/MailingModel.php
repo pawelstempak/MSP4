8d775ee33a452e1717702cc18f70705045b20d4c
Paweł Stempak <pawelstempak@gmail.com>
2022-05-04 22:22:13 +0200

    msp0.0.21

--- models/MailingModel.php
+++ models/MailingModel.php
@@ -10,7 +10,7 @@ use PHPMailer\PHPMailer\Exception;
 
 class MailingModel
 {
-    public function SaveNewMailing($sender, $group, $subject, $content)
+    public function SaveNewMailing($senderId, $groupId, $subject, $content)
     {
         $db_request = Application::$core->con->pdo->prepare('
                                     INSERT INTO `archives` (id_senders, id_groups, subject, content)
@@ -18,52 +18,91 @@ class MailingModel
         ');
         $db_request->execute(
             array(
-                "id_senders" => $sender,
-                "id_groups" => $group,
+                "id_senders" => $senderId,
+                "id_groups" => $groupId,
                 "subject" => $subject,
                 "content" => $content
             )
         );
     }
 
+    public function getSender($senderId)
+    {
+        $db_request = Application::$core->con->pdo->prepare('
+                                                            SELECT *
+                                                            FROM senders
+                                                            WHERE id = :id
+        ');
+        $db_request->execute(
+                                array(
+                                    "id" => $senderId
+                                )
+        );
+        return $db_request->fetch(PDO::FETCH_ASSOC);
+
+    }
+
+    public function getReciepientsList($groupId)
+    {
+        $db_request = Application::$core->con->pdo->prepare('
+                                                            SELECT email, id
+                                                            FROM emails
+                                                            WHERE id_group = :id
+        ');
+        $db_request->execute(
+                                array(
+                                    "id" => $groupId
+                                )
+        );
+        return $db_request->fetchAll(PDO::FETCH_ASSOC);
+
+    }    
+
     public function SendNewMailing(array $getBody)
     {
-        $this->SaveNewMailing($getBody['id_sender'], $getBody['id_group'], $getBody['subject'], $getBody['content']);
 
-        //Create an instance; passing `true` enables exceptions
+
+        $cSender = $this->getSender($getBody['id_sender']);
+
+        $cGroups = $this->getReciepientsList($getBody['id_group']);
+
         $mail = new PHPMailer(true);
 
         try {
             //Server settings
+            $mail->CharSet = "UTF-8";
+            $mail->Encoding = 'base64';
             $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
             $mail->isSMTP();                                            //Send using SMTP
-            $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
-            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->Username   = 'user@example.com';                     //SMTP username
-            $mail->Password   = 'secret';                               //SMTP password
+            $mail->Host       = $cSender['host'];                       //Set the SMTP server to send through
+            $mail->SMTPAuth   = $cSender['smtp_auth']==1? true : false;                                   //Enable SMTP authentication
+            $mail->Username   = $cSender['username'];                     //SMTP username
+            $mail->Password   = $cSender['password'];                               //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-            $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+            $mail->Port       = $cSender['port'];                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
             //Recipients
-            $mail->setFrom('from@example.com', 'Mailer');
-            $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
-            $mail->addAddress('ellen@example.com');               //Name is optional
-            $mail->addReplyTo('info@example.com', 'Information');
-            $mail->addCC('cc@example.com');
-            $mail->addBCC('bcc@example.com');
+            $mail->setFrom($cSender['email'], $cSender['name']);
+            foreach($cGroups as $key)
+            {
+                $mail->addAddress($key['email']);               //Name is optional                
+            }
+            $mail->addReplyTo($cSender['replyto'], $cSender['name']);
 
             //Attachments
-            $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-            $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+            //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+            //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
             //Content
             $mail->isHTML(true);                                  //Set email format to HTML
-            $mail->Subject = 'Here is the subject';
-            $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+            $mail->Subject = $getBody['subject'];
+            $mail->Body    = $getBody['content'];
+            //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
             $mail->send();
-            echo 'Message has been sent';
+            
+            $this->SaveNewMailing($getBody['id_sender'], $getBody['id_group'], $getBody['subject'], $getBody['content']);
+
         } catch (Exception $e) {
             echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
         }
